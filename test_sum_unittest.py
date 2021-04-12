18580900f6251e96e7508db02f1b695ad6767109
Paulo Ferraz <paulosgf.pf@gmail.com>
2021-04-12 11:02:56 -0300

    second test

--- test_sum_unittest.py
+++ test_sum_unittest.py
@@ -8,7 +8,7 @@ class TestSum(unittest.TestCase):
         """
         Test that it can sum a list of integers
         """
-        data = [1, 2, 3, 4]
+        data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
 
